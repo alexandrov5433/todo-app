7da337ad10dd8ddfe09064124777930980de84b0
alexandrov5433 <alexandrov5433@gmail.com>
2024-11-06 19:20:02 +0200

    Task deletion functionality added.

--- src/app/delete/delete.component.ts
+++ src/app/delete/delete.component.ts
@@ -1,11 +1,33 @@
 import { Component } from "@angular/core";
+import { ItemService } from "../items.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: 'app-delete',
     standalone: true,
     templateUrl: './delete.component.html',
     styleUrl: './delete.component.css',
+    providers: [ItemService]
 })
 export class DeleteComponent {
-    
+    id: string | undefined;
+
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
+        private itemService: ItemService
+    ) {
+        this.id = this.activatedRoute.snapshot.paramMap.get('id') ?? '';
+    }
+
+    confirmDelete() {
+        console.log(this.id);
+        
+        this.itemService.deleteTaskById(this.id ?? '');
+        this.router.navigate(['']);
+    }
+
+    cancelDelete() {
+        this.router.navigate(['']);
+    }
 }
